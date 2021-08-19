3930a453645f36b49bdeffeb4fd036e2db58ec69
Febrian Setianto <febrian.setianto@gmail.com>
2021-08-19 14:28:54 +0300

    randomize data

--- http-server/app/main.py
+++ http-server/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from data.predictions_handler import get_prediction
+import random
 
 
 # initiate API
@@ -24,10 +25,10 @@ async def root():
 async def predict():
     # prepare the input, just dummy for now
     data = {
-        "variance_of_wavelet": 0.5,
-        "skewness_of_wavelet": 0.5,
-        "curtosis_of_wavelet": 0.5,
-        "entropy_of_wavelet": 0.5,
+        "variance_of_wavelet": random.uniform(0, 1),
+        "skewness_of_wavelet": random.uniform(0, 1),
+        "curtosis_of_wavelet": random.uniform(0, 1),
+        "entropy_of_wavelet": random.uniform(0, 1),
     }
 
     # make predictions based on the incoming data and
@@ -39,6 +40,7 @@ async def predict():
         "predicted_class": round(float(preds.flatten())),
         "predicted_probability": float(preds.flatten()),
     }
+
 
 @app.get("/api/model/load")
 async def load_model(path: str):
